4ba0dec6fa2a49237b01962b2dbba8f1caaf6902
Guilherme Freitas <hfdsguilherme@gmail.com>
2020-08-14 18:38:12 -0300

    First commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Code\Validator\Cnpj;
+use Code\Validator\Cpf;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        \Validator::extend('document_number', function ($attribute, $value, $parameters, $validator) {
+            $documentValidator = $parameters[0] == 'cpf' ? new Cpf() : new Cnpj();
+            return $documentValidator->isValid($value);
+        });
+    }
+}
